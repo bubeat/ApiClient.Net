f9874b8d254595a542dfea0fd1b5ce0188f288e0
Mihai Mogosanu <mike@sapiensworks.com>
2015-07-29 18:27:30 -0400

    updated site

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp      = require('gulp');
 var webserver = require('gulp-webserver');
+var sass= require('gulp-sass');
 
 gulp.task('webserver', function() {
   gulp.src( '.' )
@@ -9,4 +10,14 @@ gulp.task('webserver', function() {
       livereload:       true,
       directoryListing: false
     }));
+});
+
+gulp.task('css',function(){
+  return gulp.src('./*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('default',function() {
+    gulp.watch('./*.scss',['css']);
 });
